2b672b5016f27ac1913965d254c59387b5cdcba6
jcdfusion <jaime.cortes@upr.edu>
2018-01-08 19:11:24 -0400

    Added dao and handler files

---
+++ dao/collectionCenter.py
@@ -0,0 +1,7 @@
+from config.dbconfig import pg_config
+import psycopg2
+class ResourcesDAO:
+    def __init__(self):
+
+        connection_url = "host=%s dbname=%s user=%s password=%s port=%s" % (pg_config['host'], pg_config['dbname'], pg_config['user'], pg_config['passwd'], pg_config['port'])
+        self.conn = psycopg2._connect(connection_url)
