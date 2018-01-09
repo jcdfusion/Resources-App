abf1a7319eefdbb3488f54c36cb2fbedb5d7345d
jcdfusion <jaime.cortes@upr.edu>
2018-01-08 21:44:37 -0400

    Added dao and handler files

--- dao/resources.py
+++ dao/resources.py
@@ -8,7 +8,7 @@ class ResourcesDAO:
 
     def getAllResorces(self):
         cursor = self.conn.cursor()
-        query = "select * from resourceType;"
+        query = "select * from resources;"
         cursor.execute(query)
         result = []
         for row in cursor:
@@ -17,21 +17,48 @@ class ResourcesDAO:
 
     def getResourceById(self, rid):
         cursor = self.conn.cursor()
-        query = "select * from resourceType;"
-        cursor.execute(query)
-        result = []
-        for row in cursor:
-            result.append(row)
+        query = "select * from resources where resourceID = %s;"
+        cursor.execute(query, (rid,))
+        result = cursor.fetchone()
         return result
 
-    def getResourceByName(self, rname):
+    def getResourceByCollectionCenterID(self, ccid):
         cursor = self.conn.cursor()
-        query = "select * from resourceType;"
-        cursor.execute(query)
+        query = "select * from resources where collectionCenterID = %s;"
+        cursor.execute(query, (ccid,))
+        result = cursor.fetchone()
+        return result
+
+    def getResourceByResourceTypeID(self, rtid):
+        cursor = self.conn.cursor()
+        query = "select * from resources where resourceTypeID = %s;"
+        cursor.execute(query, (rtid,))
+        result = cursor.fetchone()
+        return result
+
+    def getResourceByPurchasedFree(self, rfree):
+        cursor = self.conn.cursor()
+        query = "select * from resources where purchased_free = %s;"
+        cursor.execute(query, (rfree,))
         result = []
         for row in cursor:
             result.append(row)
         return result
 
+    def getResourceByResourcePrice(self, rprice):
+        cursor = self.conn.cursor()
+        query = "select * from resources where resource_price = %s;"
+        cursor.execute(query, (rprice,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
 
-
+    def getResourceByQty(self, rqty):
+        cursor = self.conn.cursor()
+        query = "select * from resources where qty = %s;"
+        cursor.execute(query, (rqty,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
