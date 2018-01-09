43d5865595458d1953b278e329b9ea8d867191f8
jcdfusion <jaime.cortes@upr.edu>
2018-01-08 20:17:09 -0400

    Added dao and handler files

--- handler/collectionCenter.py
+++ handler/collectionCenter.py
@@ -1,2 +1,12 @@
 from flask import jsonify
-from dao.resources import CollectionCenterDAO
+from dao.collectionCenter import CollectionCenterDAO
+
+class CollectionCenterHandler:
+
+    def build_collectionCenter_dict(self, row):
+        result = {}
+        result['ccid'] = row[0]  # collection center id
+        result['zipCode'] = row[1]  # zipCode
+        result['street'] = row[2]  # street where collection center is located
+        result['ccname'] = row[3]  # collection center name
+        return result
