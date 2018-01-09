6efa48d1513ac75d4f60614cd8c0e5fafd7ae6e5
Gustavo Andres <gamar92@users.noreply.github.com>
2018-01-08 22:09:27 -0400

    updated schema AWS

--- schema.sql
+++ schema.sql
@@ -1,98 +1,68 @@
 --This file contains the definitions of the tables used in the application
---
---
---Town table
-create table town(townID serial primary key, town_name varchar(20));
---
---State/Region table
-create table state_region(state_regionID serial primary key, townID integer references town(townID), state_region_name varchar(20)  );
---
---Country table
-create table country(countryID serial primary key, state_regionID integer references state_region(state_regionID), country_name varchar(20));
---
---Location table
-create table location(zipCode integer primary key,countryID integer references country(countryID), state_regionID integer references state_region(state_regionID), townID integer references town(townID) );
---
+--***** NEW AWS DB ******
 --Collection Center table
-create table collectionCenter(collectionCenterID serial primary key, zipCode integer references location(zipCode), street varchar(20),collection_center_name varchar(20));
---
---Resource type table
-create table resourceType(resourceTypeID serial primary key, resource_type varchar(20));
+create table collectionCenter(collectionCenterID serial primary key, zipCode integer, street varchar(20), town varchar(20), state_region varchar(20), country varchar(20), collection_center_name varchar(20));
 --
 --Resources table
-create table resources(resourceID serial primary key, collectionCenterID integer references collectionCenter(collectionCenterID), resourceTypeID integer references resourceType(resourceTypeID), purchased_free varchar(9), resource_price integer,qty integer);
+create table resources(resourceID serial primary key, collectionCenterID integer references collectionCenter(collectionCenterID), resourceType varchar(20), buy_free boolean, market_price money,qty integer);
 --
+
 --User type table
 create table userType(userTypeID serial primary key, user_type varchar(20));
 --
 --Users table
-create table users(userID serial primary key, userTypeID integer references userType(userTypeID), zipCode integer references location(zipCode), user_first_name varchar(20), user_last_name varchar(20), user_email varchar(20));
+create table users(userID varchar(20) primary key, userTypeID integer references userType(userTypeID), user_first_name varchar(20), user_last_name varchar(20), user_email varchar(20));
 --
 --Users password table
-create table userPassword(userPassID serial primary key, userID integer references users(userID), user_value varchar(20));
+create table userPassword(userPassID serial primary key, userID varchar references users(userID), user_value varchar(20));
 --
 --User Request table
-create table userRequest(requestID serial primary key, userID integer references users(userID),resourceID integer references resources(resourceID), req_first_name varchar(20),req_last_name varchar(20), req_email varchar(20), req_phone varchar(20), street varchar(20), zipCode integer, town_name varchar(20), state_region_name varchar(20), country varchar(20));
---
---Water type table
-create table waterType(waterTypeID serial primary key, water_type varchar(20));
+create table userRequest(requestID serial primary key, userID varchar references users(userID),resourceID integer references resources(resourceID), req_first_name varchar(20),req_last_name varchar(20), req_email varchar(20), req_phone varchar(20), street varchar(20), zipCode integer, town_name varchar(20), state_region_name varchar(20), country varchar(20));
 --
 --Water resource table
-create table water(waterID serial primary key, resourceID integer references resources(resourceID), waterTypeID integer references waterType(waterTypeID), qty integer);
---
---Food type table
-create table foodType(foodTypeID serial primary key, food_type varchar(20));
+create table water(waterID serial primary key, resourceID integer references resources(resourceID), waterType varchar(20), waterBrand varchar(20), qtyMeasure real);
 --
 --Food resource table
-create table food(foodID serial primary key, resourceID integer references resources(resourceID), foodTypeID integer references foodType(foodTypeID), qty integer);
+create table food(foodID serial primary key, resourceID integer references resources(resourceID), foodTypeID varchar(20), foodName varchar(20), qtyWeight real);
 --
 --Ice resource table
-create table ice(iceID serial primary key, resourceID integer references resources(resourceID), qty integer);
+create table ice(iceID serial primary key, resourceID integer references resources(resourceID), iceBrand varchar(20), qtyWeight real);
 --
---Clothing type table
-create table clothingType(clothingTypeID serial primary key, clothing_type varchar(20));
 --
 --Clothing resource table
-create table clothing(clothingID serial primary key, resourceID integer references resources(resourceID),clothingTypeID integer references clothingType(clothingTypeID),clothing_size varchar(20),qty integer);
+create table clothing(clothingID serial primary key, resourceID integer references resources(resourceID),clothingType varchar(20),clothing_size varchar(20),clothingBrand varchar(20), clothingColor varchar(20), qty real);
 --
---Gas type table
-create table gasType(gasTypeID serial primary key, gas_type varchar(20));
 --
 --Gas resource table
-create table gas(gasID serial primary key, resourceID integer references resources(resourceID), gasTypeID integer references gasType(gasTypeID), qty integer );
---
---Medical devices type table
-create table medicalDeviceType(medicalDeviceTypeID serial primary key, medicalDevice_type varchar(20));
+create table gas(gasID serial primary key, resourceID integer references resources(resourceID), gasTypeID varchar(20), gasBrand varchar(20), gasOctanage real, qty real);
+
 --
 --Medical devices table
-create table medicalDevices(medicalDevicesID serial primary key, resourceID integer references resources(resourceID), medicalDeviceTypeID integer references medicalDeviceType(medicalDeviceTypeID), medDevName varchar(20), medDevManufacturer varchar(20),qty integer);
---
---Heavy equipment type table
-create table heavyEquipmentType(heavyEquipmentTypeID serial primary key, heavyEquipment_type varchar(20));
+create table medicalDevices(medicalDevicesID serial primary key, resourceID integer references resources(resourceID), medicalDeviceType varchar(20), medDevName varchar(20), medDevManufacturer varchar(20),toTreat varchar(20), qty integer);
 --
 --Heavy equipment table
-create table heavyEquipment(heavyEquipmentID serial primary key, resourceID integer references resources(resourceID), heavyEquipmentTypeID integer references heavyEquipmentType(heavyEquipmentTypeID), qty integer);
---
---Tool type table
-create table toolType(toolTypeID serial primary key, tool_type varchar(20));
+create table heavyEquipment(heavyEquipmentID serial primary key, resourceID integer references resources(resourceID), heavyEquipmentType varchar(20), heavyEquipmentBrand varchar(20), qty real);
 --
 --Tools resource table
-create table tools(toolsID serial primary key, resourceID integer references resources(resourceID), toolTypeID integer references toolType(toolTypeID), qty integer);
+create table tools(toolsID serial primary key, resourceID integer references resources(resourceID), toolType varchar(20), toolBrand varchar(20), qty integer);
 --
---Power generator type table
-create table powerGeneratorType(powerGeneratorTypeID serial primary key, powerGenerator_type varchar(20));
---
+
 --Power generators table
-create table powerGenerator(powerGeneratorID serial primary key, resourceID integer references resources(resourceID), powerGeneratorTypeID integer references powerGeneratorType(powerGeneratorTypeID), watts integer, qty integer);
+create table powerGenerator(powerGeneratorID serial primary key, resourceID integer references resources(resourceID), powerGeneratorType varchar(20), powerGeneratorBrand varchar(20), watts real, qty integer);
 --
---Battery type table
-create table batteryType(batteryTypeID serial primary key, batteries_type varchar(20));
+
 --
 --Batteries resource table
-create table batteries(batteriesID serial primary key, resourceID integer references resources(resourceID), batteryTypeID integer references batteryType(batteryTypeID), qty integer);
---
---Medicine category table
-create table medicineCategory(medicine_categoryID serial primary key, medicine_type varchar(20));
+create table batteries(batteriesID serial primary key, resourceID integer references resources(resourceID), batteryType varchar(20), batteryBrand varchar(20), batteryVoltage real, qty integer);
+
 --
 --Medicine resource table
-create table medicine(medicineID serial primary key, resourceID integer references resources(resourceID), medicine_categoryID integer references medicineCategory(medicine_categoryID), medicine_name varchar(20), medicine_exp_date date, medicine_manufacture varchar(20));
+create table medicine(medicineID serial primary key, resourceID integer references resources(resourceID), medicineType varchar(20), medicine_form varchar(20), medicine_name varchar(20), medicine_exp_date date, medicine_manufacturer varchar(20), medicineQty real);
+--
+
+--CreditCards table 
+create table creditcards(ccNumb integer primary key, userID varchar references users(userID), ccFirst char(20), ccLast char(20), expDate date, CVC integer, cType char(20));
+--
+--Purchases table
+create table purchases(purchaseID serial primary key, userID varchar references users(userID), resourcesID integer references resources(resourceID), requestID integer references userRequest(requestID), ccNumb integer references creditcards(ccNumb), resourceQty integer);
+-- 
