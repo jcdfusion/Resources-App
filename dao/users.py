88604bda3f33b91e17f08ca8954e79c5cb512466
luisroperez <luis.perez67@upr.edu>
2018-02-02 10:20:49 -0400

    Update users.py

--- dao/users.py
+++ dao/users.py
@@ -7,7 +7,7 @@ class UsersDAO:
         connection_url = "host=%s dbname=%s user=%s password=%s port=%s" % (pg_config['host'], pg_config['dbname'], pg_config['user'], pg_config['passwd'], pg_config['port'])
         self.conn = psycopg2._connect(connection_url)
 
-    def getAllUserTypes(self):
+       def getAllUserTypes(self):
         cursor = self.conn.cursor()
         query = "select * from userType;"
         cursor.execute(query)
@@ -38,49 +38,63 @@ class UsersDAO:
         cursor = self.conn.cursor()
         query = "select user_type from userType where usertypeid = %s;"
         cursor.execute(query, (type,))
-        result = cursor.fetchone()
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getUsersByID(self, uid):
         cursor = self.conn.cursor()
         query = "select * from users where userid = %s;"
         cursor.execute(query, (uid,))
-        result = cursor.fetchone()
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getUsersByTypeID(self, utid):
         cursor = self.conn.cursor()
         query = "select * from users where userTypeID = %s;"
         cursor.execute(query, (utid,))
-        result = cursor.fetchone()
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getUsersByType(self, usertype):
         cursor = self.conn.cursor()
-        query = "select * from users natural inner join user_type where user_type = %s;"
+        query = "select * from users natural inner join user_type where LOWER(user_type) = LOWER(%s);"
         cursor.execute(query, (usertype,))
-        result = cursor.fetchone()
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getUserByFirstName(self, fname):
         cursor = self.conn.cursor()
-        query = "select * from users where user_first_name = %s;"
+        query = "select * from users where LOWER(user_first_name) = LOWER(%s);"
         cursor.execute(query, (fname,))
-        result = cursor.fetchone()
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getUserByLastName(self, lname):
         cursor = self.conn.cursor()
-        query = "select * from users where user_last_name = %s;"
+        query = "select * from users where LOWER(user_last_name) = LOWER(%s);"
         cursor.execute(query, (lname,))
-        result = cursor.fetchone()
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getUserByEmail(self, uemail):
         cursor = self.conn.cursor()
         query = "select * from users where user_email = %s;"
         cursor.execute(query, (uemail,))
-        result = cursor.fetchone()
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getAllRequests(self):
@@ -101,11 +115,112 @@ class UsersDAO:
             result.append(row)
         return result
 
+    def getUserRequestByFirstName(self,firstname):
+        cursor = self.conn.cursor()
+        query = "select * from userRequest where LOWER(req_first_name) = LOWER(%s);"
+        cursor.execute(query, (firstname,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getUserRequestByLastName(self,lastname):
+        cursor = self.conn.cursor()
+        query = "select * from userRequest where LOWER(req_last_name) = LOWER(%s);"
+        cursor.execute(query, (lastname,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+    def getUserRequestByEmail(self,email):
+        cursor = self.conn.cursor()
+        query = "select * from userRequest where LOWER(req_email) = LOWER(%s);"
+        cursor.execute(query, (email,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getUserRequestByResourceID(self,resourceid):
+        cursor = self.conn.cursor()
+        query = "select * from userRequest where resourceid = %s;"
+        cursor.execute(query, (resourceid,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getUserRequestByPhone(self,phone):
+        cursor = self.conn.cursor()
+        query = "select * from userRequest where req_phone = %s;"
+        cursor.execute(query, (phone,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getUserRequestByStreet(self,street):
+        cursor = self.conn.cursor()
+        query = "select * from userRequest where LOWER(street) = LOWER(%s);"
+        cursor.execute(query, (street,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getUserRequestByTown(self,town):
+        cursor = self.conn.cursor()
+        query = "select * from userRequest where LOWER(town_name) = LOWER(%s);"
+        cursor.execute(query, (town,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getUserRequestByState(self,state):
+        cursor = self.conn.cursor()
+        query = "select * from userRequest where LOWER(state_region_name) = LOWER(%s);"
+        cursor.execute(query, (state,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getUserRequestByCountry(self,country):
+        cursor = self.conn.cursor()
+        query = "select * from userRequest where LOWER(country) = LOWER(%s);"
+        cursor.execute(query, (country,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getUserRequestByZipcode(self,zipcode):
+        cursor = self.conn.cursor()
+        query = "select * from userRequest where zipcode = %s;"
+        cursor.execute(query, (zipcode,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+
+    def getUserRequestsByCollectionCenter(self, ccid):
+        cursor = self.conn.cursor()
+        query = "select * from userRequest where collectionCenterID = %s;"
+        cursor.execute(query, (ccid,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
     def getUserRequestsByRequestID(self, rid):
         cursor = self.conn.cursor()
         query = "select * from userRequest where requestid = %s;"
         cursor.execute(query, (rid,))
-        result = cursor.fetchone()
+        result = []
+        for row in cursor:
+            result.append(row)
         return result
 
     def getUserRequestsByUserIDandResourceID(self, uid, rid):
